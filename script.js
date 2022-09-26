84eb9dbd381772e64e9380a3a9a49373ee98e6ef
Krishna47ak <anandakrishna720@gmail.com>
2022-09-26 23:49:06 +0530

    Added Javascript to update sender name and amount

--- script.js
+++ script.js
@@ -6,18 +6,17 @@ for (i = 0; i < closebtns.length; i++) {
   closebtns[i].addEventListener("click", function() {
     this.parentElement.style.display = 'none';
   });
-}
-
+};
 // To copy text to clipboard
 function copyText() {
     navigator.clipboard.writeText
         ("DBTR/222201998174");
-}
+};
 
 
 // Updating date and time
-var date_Time = document.getElementsByClassName("dateTime")
-var date = new Date()
+var date_Time = document.getElementsByClassName("dateTime");
+var date = new Date();
 const monthNames = ["Jan","Feb","March","April","May","June","July","Aug","Sept","Oct","Nov","Dec"];
 
 function formatAMPM(date) {
@@ -29,10 +28,16 @@ function formatAMPM(date) {
   minutes = minutes < 10 ? '0'+minutes : minutes;
   var strTime = hours + ':' + minutes  + ampm;
   return strTime;
-}
+};
 
 for (let i = 0; i < date_Time.length; i++) {
   document.getElementsByClassName("dateTime")[i].innerHTML = `${date.getDate()} ${monthNames[date.getMonth()]} ${date.
     getFullYear()}, ${formatAMPM(date)} `
-}
+};
+
+// Updating transfer amount
+var transferredAmount = document.getElementById("transAmount");
+
+// Updating sender's name
+var fromName = document.getElementById("from");
 
