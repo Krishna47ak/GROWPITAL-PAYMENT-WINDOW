59525581905d6ba5df1dfe7027a226267e086ffa
Krishna47ak <anandakrishna720@gmail.com>
2022-09-26 23:34:16 +0530

    Added Javascript to update date and time

--- script.js
+++ script.js
@@ -15,5 +15,24 @@ function copyText() {
 }
 
 
+// Updating date and time
+var date_Time = document.getElementsByClassName("dateTime")
+var date = new Date()
+const monthNames = ["Jan","Feb","March","April","May","June","July","Aug","Sept","Oct","Nov","Dec"];
 
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes  + ampm;
+  return strTime;
+}
+
+for (let i = 0; i < date_Time.length; i++) {
+  document.getElementsByClassName("dateTime")[i].innerHTML = `${date.getDate()} ${monthNames[date.getMonth()]} ${date.
+    getFullYear()}, ${formatAMPM(date)} `
+}
 
